b030af08f105251953d33daead93837a107b53b2
J-P Nurmi <jpnurmi@gmail.com>
2025-07-14 16:15:29 +0200

    .

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ if [[ "$(uname -m)" != "x86_64" ]]; then
     sudo mkdir -p /__e/node20/bin
     sudo ln -s /usr/bin/node /__e/node20/bin/node
     sudo ln -s /usr/bin/npm /__e/node20/bin/npm
-    sudo sed -i 's/ID=alpine/ID=unknown/' /etc/os-release
+    sudo sed -i 's/ID=alpine/ID=unknown/' /usr/lib/os-release
 fi
 
 exec "$@"
